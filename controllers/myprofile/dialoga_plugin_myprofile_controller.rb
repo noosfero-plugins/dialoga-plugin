f15d572167ea9402a41a351288217319307ca129
Leandro Nunes dos Santos <leandronunes@gmail.com>
2015-08-05 19:07:47 -0300

    adding new method to run script

--- controllers/myprofile/dialoga_plugin_myprofile_controller.rb
+++ controllers/myprofile/dialoga_plugin_myprofile_controller.rb
@@ -1,9 +1,17 @@
 class DialogaPluginMyprofileController < MyProfileController
 
 #  before_filter :check_permission
+#  protect 'edit_profile', :profile, :only => [:set_home_page]
 
-  def sent_report
-    report = params[:report] || 
+
+  def send_report
+#    Dir
+    path = File.join(Rails.root, 'script','sent_proposal_report')
+    out = IO.popen(path)
+     raise out.readlines.inspect
+#profile.name
+#render :text => 'bli'
+#    report = params[:report] || 
 #    @article.comment_paragraph_plugin_activate = !@article.comment_paragraph_plugin_activate
 #    @article.save!
 #    redirect_to @article.view_url
@@ -11,9 +19,9 @@ class DialogaPluginMyprofileController < MyProfileController
 
   protected
 
-  def check_permission
+#  def check_permission
 #    @article = profile.articles.find(params[:id])
 #    render_access_denied unless @article.comment_paragraph_plugin_enabled? && @article.allow_edit?(user)
-  end
+#  end
 
 end
