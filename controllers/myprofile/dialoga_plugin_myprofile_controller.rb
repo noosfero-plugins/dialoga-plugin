14cab1934b678869e16e9e0943d0e7a93baaba21
Leandro Nunes dos Santos <leandronunes@gmail.com>
2015-08-06 08:34:37 -0300

    updating scritps reports

--- controllers/myprofile/dialoga_plugin_myprofile_controller.rb
+++ controllers/myprofile/dialoga_plugin_myprofile_controller.rb
@@ -1,27 +1,16 @@
 class DialogaPluginMyprofileController < MyProfileController
 
-#  before_filter :check_permission
-#  protect 'edit_profile', :profile, :only => [:set_home_page]
-
+  protect 'perform_task', :profile
 
   def send_report
-#    Dir
-    path = File.join(Rails.root, 'script','sent_proposal_report')
-    out = IO.popen(path)
-     raise out.readlines.inspect
-#profile.name
-#render :text => 'bli'
-#    report = params[:report] || 
-#    @article.comment_paragraph_plugin_activate = !@article.comment_paragraph_plugin_activate
-#    @article.save!
-#    redirect_to @article.view_url
+    path = File.join(Rails.root,'plugins','dialoga','script')
+    scripts = ['sent_event_report', 'sent_proposal_report']
+    scripts.map do |script|
+      cmd = File.join(path,script) + ' ' + current_person.email.to_s
+      fork {IO.popen(cmd).read}
+    end
+    flash[:notice] = _("The report wil be sent to email #{current_person.email}")
+    redirect_to :back
   end
-
-  protected
-
-#  def check_permission
-#    @article = profile.articles.find(params[:id])
-#    render_access_denied unless @article.comment_paragraph_plugin_enabled? && @article.allow_edit?(user)
-#  end
 
 end
