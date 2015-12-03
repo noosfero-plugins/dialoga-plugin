5399368c0bb7a313d4df4f4e0b5c26a8dd714123
Victor Costa <vfcosta@gmail.com>
2015-12-03 10:55:22 -0300

    Refactor validation of abstract max length

--- lib/ext/proposal.rb
+++ lib/ext/proposal.rb
@@ -1,11 +1,7 @@
 require_relative '../../../proposals_discussion/lib/proposals_discussion_plugin/proposal.rb'
 
-class ProposalsDiscussionPlugin::Proposal < TinyMceArticle
-  validate :abstract_max_length
+class ProposalsDiscussionPlugin::Proposal
 
-  def abstract_max_length
-    if abstract.size > 200 and environment.plugin_enabled? "Dialoga"
-      errors.add(:proposal, "Proposal must be no longer than 200 characters")
-    end
-  end
+  validates :abstract, length: {maximum: 200}, if: proc { |a| a.environment.plugin_enabled?(DialogaPlugin)}
+
 end
